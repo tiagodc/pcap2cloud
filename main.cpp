1279c21b5d1164024771ce75972bfd1f32bfac12
Tiago de Conto <tdc.florestal@gmail.com>
2017-12-14 05:46:22 -0200

    cloud parser]

--- main.cpp
+++ main.cpp
@@ -9,10 +9,27 @@
 #include <cstddef>
 #include <stdlib.h>
 #include <stdio.h>
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/replace.hpp>
+#include "laswriter.hpp"
+
+#define PI 3.14159265358979323846264338327950288
 
 using namespace std;
 
-typedef string hexVector;
+class dataBlock{
+    public:
+        int timeStamp;
+        float azimuth;
+        float azimuth2;
+        vector<float> distances;
+        vector<int> reflectivities;
+        vector<int> verticalAngles;
+        vector<int> blocks;
+        vector<float> X;
+        vector<float> Y;
+        vector<float> Z;
+};
 
 string ToHex(const string& s, bool upper_case, string between = ""){
     ostringstream ret;
@@ -74,31 +91,215 @@ string hexToText(string& hex){
     return newString;
 }
 
-vector<string> packetSplitter(string& rawPcap){
+dataBlock packetParser(string& packet){
 
-    int len = rawPcap.length();
-    vector<string> packetHolder;
+    vector<float> distances;
+    vector<int> reflectivities;
+    vector<int> vAngles;
+    vector<int> blocks;
 
-    for(int i = 0; i < len; i+=2){
-        string byte = rawPcap.substr(i,4);
+    int timeStamp = -1;
 
-        if(byte == "ffee"){
-            packetHolder.push_back( rawPcap.substr(i, 105) );
+    if(packet.length() > 196){
+        string revBytes = packet.substr(202,2) + packet.substr(200,2) + packet.substr(198,2) + packet.substr(196,2);
+        timeStamp = strtol(revBytes.c_str(), nullptr, 16);
+    }
+
+    float azimuth = (float)strtol( (packet.substr(2,2) + packet.substr(0,2)).c_str() , nullptr, 16) / 100;
+
+    int laserId = 0;
+    int block = 1;
+    for(int i = 4; i < 196; i+=6){
+        string hexDist = packet.substr(i+2,2) + packet.substr(i,2);
+        float dist = (float)(2 * std::strtol(hexDist.c_str(), nullptr, 16)) / 1000;
+
+        distances.push_back(dist);
+
+        int reflect = std::strtol( packet.substr(i+4,2).c_str() , nullptr, 16);
+        reflectivities.push_back(reflect);
+
+        int vAng = (laserId % 2 == 0) ? (laserId - 15) : laserId;
+        vAngles.push_back(vAng);
+
+        blocks.push_back(block);
+
+        if(++laserId > 15){
+            laserId = 0;
+            block++;
         }
     }
 
-    return packetHolder;
+    dataBlock temp;
+    temp.azimuth = azimuth;
+    temp.timeStamp = timeStamp;
+    temp.distances = distances;
+    temp.reflectivities = reflectivities;
+    temp.verticalAngles = vAngles;
+    temp.blocks = blocks;
+
+    return temp;
 
 }
 
+void calcXYZ(dataBlock& db, float clipOut = 0.5){
+
+    db.X.resize(0);
+    db.Y.resize(0);
+    db.Z.resize(0);
+
+    for(int i = 0; i < db.distances.size(); ++i){
+        float az = ( (db.blocks[i] == 1) ? db.azimuth : db.azimuth2 );
+        //float zRange = ( (db.verticalAngles[i] < 0) ? -1 : 1 );
+
+        db.X.push_back( db.distances[i] * cos( (float)db.verticalAngles[i] * PI / 180 ) * sin( az * PI / 180 ) );
+        db.Y.push_back( db.distances[i] * cos( (float)db.verticalAngles[i] * PI / 180 ) * cos( az * PI / 180 ) );
+        db.Z.push_back( db.distances[i] * sin( (float)db.verticalAngles[i] * PI / 180 ) );
+    }
+
+    for(int i = db.X.size()-1; i >=0; --i){
+        if(abs(db.X[i]) < clipOut || abs(db.Y[i]) < clipOut || abs(db.Z[i]) < clipOut){
+            db.blocks.erase(db.blocks.begin()+i);
+            db.distances.erase(db.distances.begin()+i);
+            db.reflectivities.erase(db.reflectivities.begin()+i);
+            db.verticalAngles.erase(db.verticalAngles.begin()+i);
+            db.X.erase(db.X.begin()+i);
+            db.Y.erase(db.Y.begin()+i);
+            db.Z.erase(db.Z.begin()+i);
+        }
+    }
+
+}
+
+class VLPcloud{
+    public:
+        vector<dataBlock> dataBlocks;
+
+        void build(char* fileName){
+            dataBlocks.resize(0);
+            string fileString = hexStringMaker(fileName);
+            boost::replace_all(fileString, "ffee", " ");
+
+            std::vector<std::string> packets = stringSplitter(fileString);
+
+            for(int p = 1; p < packets.size(); ++p){
+                dataBlock pack = packetParser(packets[p]);
+                dataBlocks.push_back(pack);
+            }
+        }
+
+        void calculate(){
+
+            int stampIndex = -1;
+            int firstUntimed = 0;
+
+            for(int i = 0; i < dataBlocks.size(); ++i){
+                dataBlock& temp = dataBlocks[i];
+
+                //timestamps for all (without interpolation)
+                if(temp.timeStamp > -1 && temp.timeStamp != stampIndex){
+                    for(int j = firstUntimed ; j < i; ++j){
+                        dataBlock& temp2 = dataBlocks[j];
+                        temp2.timeStamp = temp.timeStamp;
+                    }
+                }else if(temp.timeStamp == -1 && temp.timeStamp != stampIndex){
+                    firstUntimed = i;
+                }
+
+                //interpolate azimuths and calculate coordinates
+                if(i > 0){
+                    dataBlock& temp0 = dataBlocks[i-1];
+                    float az0 = temp0.azimuth;
+                    float az1 = temp.azimuth;
+
+                    temp0.azimuth2 = (az0 + az1) / 2;
+
+                    calcXYZ(temp);
+                }
+
+                stampIndex = temp.timeStamp;
+            }
+
+            calcXYZ(dataBlocks[0]);
+        }
+
+
+        void write(char* fileName, bool shiftZ = false){
+
+            string file(fileName);
+            string extension = file.substr(file.size()-3, 3);
+            boost::algorithm::to_lower(extension);
+
+            if(extension == "las" || extension == "laz"){
+
+                int format_macro = ((extension == "laz") ? LAS_TOOLS_FORMAT_LAZ : LAS_TOOLS_FORMAT_LAS);
+
+                LASwriteOpener laswriteopener;
+                laswriteopener.set_file_name(fileName);
+                laswriteopener.set_format(format_macro);
+
+                LASheader lasheader;
+                lasheader.point_data_format = 0;
+                lasheader.point_data_record_length = 20;
+                LASpoint laspoint;
+                laspoint.init(&lasheader, lasheader.point_data_format, lasheader.point_data_record_length, &lasheader);
+                LASwriter* laswriter = laswriteopener.open(&lasheader);
+
+                vector<dataBlock>* coordinates = &dataBlocks;
+                vector<dataBlock>::iterator packet;
+
+                packet = coordinates->begin();
+                while (packet != coordinates->end()){
+
+                    for(int pt = 0; pt < packet->blocks.size(); ++pt){
+                        laspoint.set_x( packet->X[pt] );
+                        laspoint.set_y( ((shiftZ) ? packet->Z[pt] : packet->Y[pt]) );
+                        laspoint.set_z( ((shiftZ) ? packet->Y[pt] : packet->Z[pt]) );
+                        laspoint.set_intensity( packet->reflectivities[pt] );
+                        laspoint.set_gps_time( packet->timeStamp );
+                        //laspoint.set_user_data( packet->timeStamp );
+
+                        laswriter->write_point(&laspoint);
+                        laswriter->update_inventory(&laspoint);
+                    }
+                    packet++;
+                }
+
+                laswriter->update_header(&lasheader, TRUE);
+                laswriter->close(TRUE);
+
+                delete laswriter;
+
+            }else{
+
+                ofstream myfile;
+                myfile.open (fileName);
+                myfile << "timeStamp block azimuth verticalAngle distance x y z reflectivity\n";
+                for(int p = 1; p < dataBlocks.size(); ++p){
+                    dataBlock pack = dataBlocks[p];
+                    for(int i = 0; i < pack.distances.size(); ++i){
+                        myfile <<
+                        pack.timeStamp << " " <<
+                        pack.blocks[i] << " " <<
+                        ((pack.blocks[i] == 1) ? pack.azimuth : pack.azimuth2) << " " <<
+                        pack.verticalAngles[i] << " " <<
+                        pack.distances[i] << " " <<
+                        pack.X[i] << " " <<
+                        ((shiftZ) ? pack.Z[i] : pack.Y[i]) << " " <<
+                        ((shiftZ) ? pack.Y[i] : pack.Z[i]) << " " <<
+                        pack.reflectivities[i] << "\n";
+                    }
+                }
+                myfile.close();
+            }
+        }
+};
 
 int main(){
 
-    string fileString = hexStringMaker("n24.pcap");
-
-    vector<string> packets = packetSplitter(fileString);
-
-    cout << packets[0] << endl;
+    VLPcloud nuvem;
+    nuvem.build("n200.pcap");
+    nuvem.calculate();
+    nuvem.write("pcap.txt");
 
     return 0;
 }
